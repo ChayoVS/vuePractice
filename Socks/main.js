35795903093be1fbd0c01471fe483d3b13a81df5
rosario VS <paradisekiss-yaoi@hotmail.com>
2020-03-16 11:16:37 -0600

    refactorizando para utilizar la propiedad computing en Vue

--- Socks/main.js
+++ Socks/main.js
@@ -1,22 +1,24 @@
 const app = new Vue ({
     el: '#app',
     data: {
+        brand: 'Vue Mastery',
         product: 'Socks',
         description: 'Keep your feet warm with this pair of beautiful socks',
-        image: './img/vmSocks-green-onWhite.jpg',
+        selectedVariant: 0,
         link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks',
-        inStock: true,
         details: ['80% cotton', '20% polyester', 'Gender-neutral'],
         variants: [
             {
                 variantId: 2234,
                 variantColor: "green",
-                variantImage: './img/vmSocks-green-onWhite.jpg'
+                variantImage: './img/vmSocks-green-onWhite.jpg',
+                variantQuantity: 10
             },
             {
                 variantId: 2235,
                 variantColor: "blue",
-                variantImage: './img/vmSocks-blue-onWhite.jpg'
+                variantImage: './img/vmSocks-blue-onWhite.jpg',
+                variantQuantity: 0,
             }
         ],
 
@@ -32,9 +34,23 @@ const app = new Vue ({
             this.cart --
         },
 
-        updateProduct(variantImage){
-            this.image = variantImage
+        updateProduct(index){
+            this.selectedVariant = index
         }
     },
+
+    computed: {
+        title(){
+            return this.brand + ' ' + this.product
+        }, 
+
+        image() {
+            return this.variants[this.selectedVariant].variantImage
+        },
+
+        inStock(){
+            return this.variants[this.selectedVariant].variantQuantity
+        }
+    }
 })
 
