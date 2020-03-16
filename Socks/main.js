fd51d26620b53b0ed9af71ddf3d11ca30d6b55f0
rosario VS <paradisekiss-yaoi@hotmail.com>
2020-03-15 21:08:57 -0600

    agregando métodos, haciendo dinámicas las imágene, usando mouseover, añadir y remover productos del carrito

--- Socks/main.js
+++ Socks/main.js
@@ -5,11 +5,36 @@ const app = new Vue ({
         description: 'Keep your feet warm with this pair of beautiful socks',
         image: './img/vmSocks-green-onWhite.jpg',
         link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks',
-        inventory: 25
+        inventory: 25,
+        details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: "green",
+                variantImage: './img/vmSocks-green-onWhite.jpg'
+            },
+            {
+                variantId: 2235,
+                variantColor: "blue",
+                variantImage: './img/vmSocks-blue-onWhite.jpg'
+            }
+        ],
+
+        cart: 0
     },
 
     methods: {
-        
+        addToCart(){
+            this.cart ++
+        },
+
+        removeToCart(){
+            this.cart --
+        },
+
+        updateProduct(variantImage){
+            this.image = variantImage
+        }
     },
 })
 
