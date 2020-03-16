fd255c2daa262bb11080fec34b1388cd2ffdc783
rosario VS <paradisekiss-yaoi@hotmail.com>
2020-03-15 18:58:18 -0600

    aprendiendo sobre condicionales en vue, links, agregar imagenes

---
+++ Socks/main.js
@@ -0,0 +1,15 @@
+const app = new Vue ({
+    el: '#app',
+    data: {
+        product: 'Socks',
+        description: 'Keep your feet warm with this pair of beautiful socks',
+        image: './img/vmSocks-green-onWhite.jpg',
+        link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks',
+        inventory: 25
+    },
+
+    methods: {
+        
+    },
+})
+
